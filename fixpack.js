14d08c90aa7640718e99b27a1f9f88cfcbb13114
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-12-16 17:41:06 +0100

    1.3.0 - Added support for package.json and bower.json by default - Support for add other files using the CLI - Minimal Changes

--- fixpack.js
+++ fixpack.js
@@ -1,58 +1,58 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var colors = require('colors');
+var fs       = require('fs');
+var colors   = require('colors');
 var required = ['name', 'version'];
-var warn = ['description', 'author', 'repository', 'keywords', 'main', 'bugs', 'homepage', 'license'];
-var ALCE = require('alce');
-var os = require('os');
+var warn     = ['description', 'author', 'repository', 'keywords', 'main', 'bugs', 'homepage', 'license'];
+var ALCE     = require('alce');
+var os       = require('os');
+var path     = require('path');
 
-
-function checkMissing(pack, log) {
-    required.forEach(function (key) {
-        if (!pack[key]) throw new Error('package.json files must have a ' + key);
-    });
-    warn.forEach(function (key) {
-        if (!pack[key] && log) console.log(('missing ' + key).yellow);
-    });
+function checkMissing(pack, log, fileName) {
+  required.forEach(function (key) {
+      if (!pack[key]) throw new Error(fileName + ' must have a ' + key);
+  });
+  warn.forEach(function (key) {
+      if (!pack[key] && log) console.log(('missing ' + key).yellow);
+  });
 }
 
 function sortObjectKeysAlphabetically(object) {
-    var sorted = {};
-    Object.keys(object).sort().forEach(function (key) {
-        sorted[key] = object[key];
-    });
-    return sorted;
+  var sorted = {};
+  Object.keys(object).sort().forEach(function (key) {
+      sorted[key] = object[key];
+  });
+  return sorted;
 }
 
 module.exports = function (file, log) {
-    var out = {};
-    var pack = ALCE.parse(fs.readFileSync(file, {encoding: 'utf8'}));
-    var key;
+  if (!fs.existsSync(file)) return;
+  var key;
+  var out = {};
+  var fileName = path.basename(file);
+  var pack = ALCE.parse(fs.readFileSync(file, {encoding: 'utf8'}));
+  // make sure we have everything
+  checkMissing(pack, true, fileName);
 
-    // make sure we have everything
-    checkMissing(pack);
+  // handle the specific ones we want, then remove
+  ['name', 'description', 'version', 'author'].forEach(function (key) {
+    if (pack[key]) out[key] = pack[key];
+    delete pack[key];
+  });
 
-    // handle the specific ones we want, then remove
-    ['name', 'description', 'version', 'author'].forEach(function (key) {
-        if (pack[key]) out[key] = pack[key];
-        delete pack[key];
-    });
+  // sort the remaining
+  pack = sortObjectKeysAlphabetically(pack);
 
-    // sort the remaining
-    pack = sortObjectKeysAlphabetically(pack);
+  // add in the sorted ones
+  for (key in pack) {
+    out[key] = pack[key];
+  }
 
-    // add in the sorted ones
-    for (key in pack) {
-        out[key] = pack[key];
-    }
+  // sort some sub items alphabetically
+  ['dependencies', 'devDependencies', 'jshintConfig', 'scripts'].forEach(function (key) {
+    if (out[key]) out[key] = sortObjectKeysAlphabetically(out[key]);
+  });
 
-    // sort some sub items alphabetically
-    ['dependencies', 'devDependencies', 'jshintConfig', 'scripts'].forEach(function (key) {
-        if (out[key]) out[key] = sortObjectKeysAlphabetically(out[key]);
-    });
-
-    // write it out
-    fs.writeFileSync(file, JSON.stringify(out, null, 2) + os.EOL, {encoding: 'utf8'});
-
-    if (log) console.log('package.json'.bold + ' fixed'.green + '!');
+  // write it out
+  fs.writeFileSync(file, JSON.stringify(out, null, 2) + os.EOL, {encoding: 'utf8'});
+  if (log) console.log(fileName.bold + ' fixed'.green + '!');
 };
