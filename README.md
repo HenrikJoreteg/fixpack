95211b5e0a3ef9a13de1dacba7e0c7cbf79373a0
Henrik Joreteg <hjoreteg@gmail.com>
2015-02-20 16:37:16 -0800

    configurable via config file

--- README.md
+++ README.md
@@ -10,17 +10,18 @@ It will re-write your package.json file as follows:
 - author fourth
 - all other keys in alphabetical order
 - dependencies and devDependencies sorted alphabetically
-- append operating system appropriate newline at the end of the file
+- newline at the end of the file
 
 It will warn you if any of these are missing:
 
+- description
 - author
 - repository
 - keywords
 - main
 - bugs
 - homepage
-- license
+- license'
 
 Fix all indenting to 2 spaces.
 
@@ -46,14 +47,68 @@ fixpack
 npm i cool_package --save && fixpack
 ```
 
+## Configuration
+
+It's configurable. You can create a `.fixpackrc` file in your project or anywhere up the tree to your `$HOME` directory. Or overwrite options via CLI arguments.
+
+Uses the [rc](https://www.npmjs.com/package/rc) module to do this. So you can pass all these as CLI args too.
+
+The available options and their defaults shown below: 
+
+```js
+
+{
+    // will put these first in this order if present
+    sortToTop: [
+        'name',
+        'description',
+        'version',
+        'author'
+    ],
+    // will error if these not present
+    required: [
+        'name',
+        'version'
+    ],
+    // will warn if these not present
+    warn: [
+        'description',
+        'author',
+        'repository',
+        'keywords',
+        'main',
+        'bugs',
+        'homepage',
+        'license'
+    ],
+    // if `private: true` in package.json will use the next two lists instead
+    requiredOnPrivate: [],
+    warnOnPrivate: ['name', 'version', 'description', 'main'],
+    // sub items to sort by default
+    sortedSubItems: [
+        'dependencies',
+        'devDependencies',
+        'jshintConfig',
+        'scripts',
+        'keywords'
+    ],
+    // if you set quiet to true it won't do output anything to the console
+    quiet: false   
+}
+
+```
+
 ## Changelog
 
+- 1.3.0 - configurable via `.fixpackrc` 
+- x.x.x - unknown miscellaneous madness and poor version tracking
 - 0.0.2 [diff](https://github.com/HenrikJoreteg/fixpack/compare/v0.0.1...v0.0.2) - EOF newline
 - 0.0.1 - initial release
 
 ## Credits
 
-This embarrassing display of insanity, type-A-ness, and OCD brought to you by [@HenrikJoreteg](http://twitter.com/henrikjoreteg).
+This embarrassing display of insanity,
+type-A-ness, and OCD brought to you by [@HenrikJoreteg](http://twitter.com/henrikjoreteg).
 
 ## License
 
--- fixpack.js
+++ fixpack.js
@@ -1,50 +1,62 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var required = ['name', 'version'];
-var warnList = ['description', 'author', 'repository', 'keywords', 'main', 'bugs', 'homepage', 'license'];
-var privateWarnList = ['description', 'main'];
 var ALCE = require('alce');
 require('colors');
 
 
-function checkMissing(pack, log) {
-    var warnItems = pack.private ? privateWarnList : warnList;
+function checkMissing(pack, config) {
+    var warnItems;
+    var required;
+    if (pack.private) {
+        warnItems = config.warnOnPrivate;
+        required = config.requiredOnPrivate;
+    } else {
+        warnItems = config.warn;
+        required = config.required;
+    }
     required.forEach(function (key) {
         if (!pack[key]) throw new Error('package.json files must have a ' + key);
     });
     warnItems.forEach(function (key) {
-        if (!pack[key] && log) console.log(('missing ' + key).yellow);
+        if (!pack[key] && !log) console.log(('missing ' + key).yellow);
     });
 }
 
-function sortObjectKeysAlphabetically(object) {
-    var sorted = {};
-    Object.keys(object).sort().forEach(function (key) {
-        sorted[key] = object[key];
-    });
-    return sorted;
+function sortAlphabetically(object) {
+    if (Array.isArray(object)) {
+        object.sort();
+        return object;
+    } else {
+        var sorted = {};
+        Object.keys(object).sort().forEach(function (key) {
+            sorted[key] = object[key];
+        });
+        return sorted;
+    }
 }
 
-module.exports = function (file, log) {
+module.exports = function (file, config) {
     if (!fs.existsSync(file)) {
-        console.log(('No such file: ' + file).red);
+        if (!config.quiet) console.log(('No such file: ' + file).red);
         process.exit(1);
     }
-    var pack = ALCE.parse(fs.readFileSync(file, {encoding: 'utf8'}));
+    var original = fs.readFileSync(file, {encoding: 'utf8'});
+    var pack = ALCE.parse(original);
     var out = {};
+    var outputString = '';
     var key;
 
     // make sure we have everything
-    checkMissing(pack);
+    checkMissing(pack, config);
 
     // handle the specific ones we want, then remove
-    ['name', 'description', 'version', 'author'].forEach(function (key) {
+    config.sortToTop.forEach(function (key) {
         if (pack[key]) out[key] = pack[key];
         delete pack[key];
     });
 
     // sort the remaining
-    pack = sortObjectKeysAlphabetically(pack);
+    pack = sortAlphabetically(pack);
 
     // add in the sorted ones
     for (key in pack) {
@@ -52,12 +64,17 @@ module.exports = function (file, log) {
     }
 
     // sort some sub items alphabetically
-    ['dependencies', 'devDependencies', 'jshintConfig', 'scripts'].forEach(function (key) {
-        if (out[key]) out[key] = sortObjectKeysAlphabetically(out[key]);
+    config.sortedSubItems.forEach(function (key) {
+        if (out[key]) out[key] = sortAlphabetically(out[key]);
     });
 
     // write it out
-    fs.writeFileSync(file, JSON.stringify(out, null, 2) + '\n', {encoding: 'utf8'});
+    outputString = JSON.stringify(out, null, 2) + '\n';
 
-    if (log) console.log('package.json'.bold + ' fixed'.green + '!');
+    if (outputString !== original) {
+        fs.writeFileSync(file, outputString, {encoding: 'utf8'});
+        if (!config.quiet) console.log('package.json'.bold + ' fixed'.green + '!');
+    } else {
+        if (!config.quiet) console.log('package.json'.bold + ' already clean'.green + '!');
+    }
 };
