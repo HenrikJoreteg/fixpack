b9ff5a8623680c8d077cbd7cbd6a3c8c9b5c2c2e
Henrik Joreteg <hjoreteg@gmail.com>
2020-02-18 05:53:54 -0800

    updated packages, minor changes

--- fixpack.js
+++ fixpack.js
@@ -3,16 +3,15 @@
 'use strict'
 
 const ALCE = require('alce')
-const extend = require('extend-object')
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 
 const defaultConfig = require('./config')
 
-function checkMissing (pack, config) {
-  var warnItems
-  var required
+function checkMissing(pack, config) {
+  let warnItems
+  let required
   if (pack.private) {
     warnItems = config.warnOnPrivate
     required = config.requiredOnPrivate
@@ -20,45 +19,48 @@ function checkMissing (pack, config) {
     warnItems = config.warn
     required = config.required
   }
-  required.forEach(function (key) {
-    if (!pack[key]) throw new Error(config.fileName + ' files must have a ' + key)
+  required.forEach(function(key) {
+    if (!pack[key])
+      throw new Error(config.fileName + ' files must have a ' + key)
   })
-  warnItems.forEach(function (key) {
+  warnItems.forEach(function(key) {
     if (!pack[key] && !config.quiet) console.log(('missing ' + key).yellow)
   })
 }
 
-function sortAlphabetically (object) {
+function sortAlphabetically(object) {
   if (Array.isArray(object)) {
     object.sort()
     return object
   } else {
-    var sorted = {}
-    Object.keys(object).sort().forEach(function (key) {
-      sorted[key] = object[key]
-    })
+    const sorted = {}
+    Object.keys(object)
+      .sort()
+      .forEach(function(key) {
+        sorted[key] = object[key]
+      })
     return sorted
   }
 }
 
-module.exports = function (file, config) {
-  config = extend(defaultConfig, config || {})
+module.exports = function(file, config) {
+  config = Object.assign(defaultConfig, config || {})
   if (!fs.existsSync(file)) {
     if (!config.quiet) console.log(('No such file: ' + file).red)
     process.exit(1)
   }
   config.fileName = path.basename(file)
-  var original = fs.readFileSync(file, { encoding: 'utf8' })
-  var pack = ALCE.parse(original)
-  var out = {}
-  var outputString = ''
-  var key
+  const original = fs.readFileSync(file, { encoding: 'utf8' })
+  const out = {}
+  let pack = ALCE.parse(original)
+  let outputString = ''
+  let key
 
   // make sure we have everything
   checkMissing(pack, config)
 
   // handle the specific ones we want, then remove
-  config.sortToTop.forEach(function (key) {
+  config.sortToTop.forEach(function(key) {
     if (pack[key]) out[key] = pack[key]
     delete pack[key]
   })
@@ -76,14 +78,19 @@ module.exports = function (file, config) {
   if (typeof out.keywords === 'string') out.keywords = [out.keywords]
 
   // sort some sub items alphabetically
-  config.sortedSubItems.forEach(function (key) {
+  config.sortedSubItems.forEach(function(key) {
     if (out[key]) out[key] = sortAlphabetically(out[key])
   })
 
   // wipe version numbers
   if (config.wipe) {
-    var versionedKeys = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
-    versionedKeys.forEach(function (key) {
+    const versionedKeys = [
+      'dependencies',
+      'devDependencies',
+      'peerDependencies',
+      'optionalDependencies'
+    ]
+    versionedKeys.forEach(function(key) {
       const depGroup = out[key]
       if (depGroup) {
         for (var item in depGroup) {
@@ -98,8 +105,12 @@ module.exports = function (file, config) {
 
   if (outputString !== original) {
     fs.writeFileSync(file, outputString, { encoding: 'utf8' })
-    if (!config.quiet) console.log(chalk.bold(config.fileName) + chalk.green(' fixed') + '!')
+    if (!config.quiet)
+      console.log(chalk.bold(config.fileName) + chalk.green(' fixed') + '!')
   } else {
-    if (!config.quiet) console.log(chalk.bold(config.fileName) + chalk.green(' already clean') + '!')
+    if (!config.quiet)
+      console.log(
+        chalk.bold(config.fileName) + chalk.green(' already clean') + '!'
+      )
   }
 }
